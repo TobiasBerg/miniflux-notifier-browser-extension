fdc75ae51cbc5494c90d4fa52848ab1c68675284
Tobias Berg <tb@tobiasberg.net>
2026-02-10 22:46:25 +0100

    fix missing trailing slash on api url

--- js/background.js
+++ js/background.js
@@ -2,93 +2,95 @@ const createUnreadCountRequest = async () => {
   const storage = await browser.storage.local.get({
     apikey: "",
     url: "",
-  });
+  })
 
   let headers = new Headers({
     "X-Auth-Token": storage.apikey,
-  });
-  let init = { method: "GET", headers };
-  let url = `${storage.url}v1/entries?status=unread&direction=desc`;
-  return new Request(url, init);
-};
+  })
+  let init = { method: "GET", headers }
+
+  const rUrl = !storage.url.endsWith("/") ? `${storage.url}/` : storage.url
+  let url = `${rUrl}v1/entries?status=unread&direction=desc`
+  return new Request(url, init)
+}
 
 const checkUnreadCount = async () => {
-  console.log("checking Miniflux unread count");
+  console.log("checking Miniflux unread count")
   const storage = await browser.storage.local.get({
     apikey: "",
     url: "",
-  });
+  })
 
   if (!storage.url || !storage.apikey) {
-    console.log('miniflux Notifier not configured, skipping unread count')
+    console.log("miniflux Notifier not configured, skipping unread count")
     return
   }
 
-  const request = await createUnreadCountRequest();
+  const request = await createUnreadCountRequest()
 
   fetch(request)
     .then((response) => {
-      return response.json();
+      return response.json()
     })
     .then(handleResponse)
     .catch((err) => {
-      console.error("error while fetching unread count", err);
-    });
-};
+      console.error("error while fetching unread count", err)
+    })
+}
 
 const handleResponse = (response) => {
   if (response && response.total) {
     console.log(`unread count fetched, ${response.total} unread`)
-    displayBrowserActionBadge(response.total.toString());
+    displayBrowserActionBadge(response.total.toString())
   } else {
-    displayBrowserActionBadge("");
+    displayBrowserActionBadge("")
   }
-};
+}
 
 const displayBrowserActionBadge = (number) => {
   browser.browserAction.setBadgeBackgroundColor({
     color: "rgba(230,230,230,1)",
-  });
+  })
 
-  const badgeText = number > 99 ? `${number}+` : `${number}`;
-  browser.browserAction.setBadgeText({ text: badgeText });
-};
+  const badgeText = number > 99 ? `${number}+` : `${number}`
+  browser.browserAction.setBadgeText({ text: badgeText })
+}
 
 const onBrowserActionClick = async () => {
-  console.log("opening Miniflux in new tab");
+  console.log("opening Miniflux in new tab")
 
   const storage = await browser.storage.local.get({
     apikey: "",
     url: "",
-  });
+  })
 
   var creating = browser.tabs.create({
     url: storage.url || browser.runtime.getURL("/options.html"),
-  });
+  })
   creating.then(
     () => {
-      console.log("opened in new");
+      console.log("opened in new")
     },
     (err) => {
-      console.log("error tabbing");
-      console.log(err);
-    }
-  );
-};
+      console.log("error tabbing")
+      console.log(err)
+    },
+  )
+}
 
 const init = async () => {
   // Check for clicks on toolbar button
-  browser.browserAction.onClicked.addListener(onBrowserActionClick);
+  browser.browserAction.onClicked.addListener(onBrowserActionClick)
 
-  checkUnreadCount();
+  checkUnreadCount()
 
   setInterval(() => {
     try {
-      checkUnreadCount();
+      checkUnreadCount()
     } catch (err) {
-      console.error("error updating count", err);
+      console.error("error updating count", err)
     }
-  }, 1000 * 60);
-};
+  }, 1000 * 60)
+}
 
-init();
+init()
